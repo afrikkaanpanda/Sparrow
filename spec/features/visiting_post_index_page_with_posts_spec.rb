2241c2cef555994508334614fc5334757a9763b2
afrikkaanpanda <alando.willo@gmail.com>
2022-08-31 13:18:04 -0500

    feature test update

--- spec/features/visiting_post_index_page_with_posts_spec.rb
+++ spec/features/visiting_post_index_page_with_posts_spec.rb
@@ -1,14 +1,15 @@
-# # frozen_string_literal: true
-# require "rails_helper"
+# frozen_string_literal: true
+require "rails_helper"
 
-# RSpec.feature "Visit Post Index Page", type: :feature do
-#     scenario "visit Post Index Page with Posts" do
-#       visit "/posts"
-#       expect(page.body).to have_css("h1.title", text: "Posts")
-#       expect(page.body).to have_text: ("Author:")
-#       expect(page.body).to have_text: ("Title:")
-#       expect(page.body).to have_text: ("Description:")
-#       expect(page.body).to have_css("button", text: "Home")
+RSpec.feature "Visit Post Index Page", type: :feature do
+    scenario "visit Post Index Page with Posts" do
+        post= Post.create(author: "", title: "", description: "")
+      visit "/posts"
+      expect(page.body).to have_css("h1.title", text: "Posts")
+      expect(page.body).to have_text(post.author)
+      expect(page.body).to have_text(post.title)
+      expect(page.body).to have_text(post.description.to_plain_text)
+      expect(page.body).to have_css("button", text: "Home")
     
-#     end
-#   end
+    end
+  end
